5aa777a0d2b6e0cd4f697c0fbb2efcce09d14fda
Utente <Utente@NBASUS.homenet.telecomitalia.it>
2021-03-10 10:24:02 +0100

    Soluzione 1 e 2

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -3,6 +3,7 @@ package it.polito.tdp.parole;
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.*;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,8 +12,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	
-	Parole elenco ;
+	Parole elenco;
 
     @FXML
     private ResourceBundle resources;
@@ -28,27 +28,76 @@ public class FXMLController {
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML
+    private TextArea txtTempo;
+    
+    @FXML
+    private Button btnCanc;
 
     @FXML
     private Button btnReset;
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	txtResult.setText("");
+    	
+    	String parola = txtParola.getText();
+    	
+    	long startTime = System.nanoTime();
+    	elenco.addParola(parola);
+    	long time =   System.nanoTime()- startTime;
+    	
+    	List<String> daStampare = elenco.getElenco();
+    	String stampa= "";
+    	for(String s: daStampare) {
+			stampa = stampa + s + "\n";
+		}
+    	txtResult.setText(stampa);
+    	txtParola.setText("");
+    	String tempo = "" + (time);
+    	txtTempo.appendText("INSERT: "+tempo + "ns"+"\n");
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	txtResult.setText("");
+    	txtTempo.setText("");
+    	elenco.reset();
     }
 
+    @FXML
+    void doCanc(ActionEvent event) {
+    	
+    	String parolaCanc = txtResult.getSelectedText();
+    	
+    	long startTime = System.nanoTime();
+    	elenco.cancella(parolaCanc);
+    	long time =   System.nanoTime()- startTime;
+    	
+    	List<String> daStampare = elenco.getElenco();
+    	String stampa= "";
+    	for(String s: daStampare) {
+			stampa = stampa + s + "\n";
+		}
+    	
+    	txtResult.setText(stampa);
+    	txtParola.setText("");
+    	String tempo = "" + (time);
+    	txtTempo.appendText("CANC: " + tempo + "ns"+"\n");
+    }
+    
     @FXML
     void initialize() {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCanc != null : "fx:id=\"btnCanc\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
-        elenco = new Parole() ;
+        elenco = new Parole();
     }
+    //getSelectedText()
 }
+
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,32 @@
 package it.polito.tdp.parole.model;
 
-import java.util.List;
+import java.util.*;
 
 public class Parole {
+	
+	List <String> lista;
 		
 	public Parole() {
-		//TODO
+		lista = new LinkedList<String>();
+		//ArrayList ci mette molto a cancellare un dato
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		lista.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(lista);
+		
+		return lista;
 	}
 	
 	public void reset() {
-		// TODO
+		lista.clear();
+	}
+	
+	public void cancella (String p) {
+		lista.remove(p);
 	}
 
 }
